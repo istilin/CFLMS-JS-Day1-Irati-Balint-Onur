a2399e6d845b51dde9dae1fb3f4a288acefc52ba
onurumar <65164611+onurumar@users.noreply.github.com>
2020-05-25 10:20:09 +0200

    intermediate push

---
+++ intermediate/script.js
@@ -0,0 +1,40 @@
+var cars= ["Tesla","Renault", "Volvo", "Mazda", "Fiat", "Ferrari"]
+
+cars.sort()
+
+console.log(cars)
+
+
+var fruits= ["Apple", "Banana", "Kiwi"]
+
+fruits.push("orange")
+
+console.log(fruits)
+
+fruits.pop()
+
+console.log(fruits)
+
+var animals= ["Monkey", "Horse", "Dog"]
+
+animals.sort()
+
+console.log(animals)
+
+animals.unshift("cat")
+
+console.log(animals)
+
+var fruits=["mango", "cherries", "kiwi", "grapes", "pear", "peach", "orange", "lemon"]
+
+console.log(fruits.join("/"))
+
+var fruit = "mango/cherries/kiwi/grapes/pear/peach/orange/lemon"
+
+
+var fruit2 = fruit.split("/")
+
+console.log(fruit2.join(" "))
+
+
+
